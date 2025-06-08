b219b346616f34bec9865a3989e938e506b2a435
coin2watch <coin2watch@gmail.com>
2025-06-08 12:40:45 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from modules import blog, shopping, instagram, youtube, search
+
+def main():
+    blog.run()
+    shopping.run()
+    instagram.run()
+    youtube.run()
+    search.run()
+
+if __name__ == "__main__":
+    main()
