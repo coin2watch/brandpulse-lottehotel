11b522b9ea23be567b28375777b3e729c082f321
coin2watch <coin2watch@gmail.com>
2025-06-08 12:59:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from modules import blog, shopping, instagram, youtube, search
+from modules.utils import safe_run
 
 def main():
-    blog.run()
-    shopping.run()
-    instagram.run()
-    youtube.run()
-    search.run()
+    safe_run("Blog", blog.run)
+    safe_run("Shopping", shopping.run)
+    safe_run("Instagram", instagram.run)
+    safe_run("Youtube", youtube.run)
+    safe_run("Search", search.run)
 
 if __name__ == "__main__":
     main()
