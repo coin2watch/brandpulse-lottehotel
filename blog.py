22a3ce07f092af087d2d7febc12267566c44da36
coin2watch <coin2watch@gmail.com>
2025-06-10 07:36:00 +0900

    Update blog.py

--- blog.py
+++ blog.py
@@ -74,3 +74,8 @@ def index():
         return "✅ BlogData updated"
     except Exception as e:
         return f"❌ Error: {e}"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+    
