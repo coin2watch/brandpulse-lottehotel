a76a7440d70a4c51b9efcc2aef2c776fd6da43f9
coin2watch <coin2watch@gmail.com>
2025-06-09 17:05:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 from blog import crawl_naver_blog
 
 if __name__ == "__main__":
-    keyword_list = ["롯데호텔", "신라호텔", "조선호텔", "베스트웨스턴"]
+    keyword_list = ['롯데호텔', '신라호텔', '조선호텔', '베스트웨스턴']
     for keyword in keyword_list:
         print(f"✅ [Blog] {keyword} 수집 중...")
         posts = crawl_naver_blog(keyword)
         for post in posts:
-            print(f"• {post['title']} | {post['link']}")
+            print(f"{post['title']} | {post['link']}")
